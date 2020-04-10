acd79ca84e9fe62895032f0407c1bcd189f06b8e
bobbysaprey420 <40564214+bobbysaprey420@users.noreply.github.com>
2020-04-11 03:24:41 +0530

    update app.js

--- app.js
+++ app.js
@@ -28,12 +28,14 @@ var questionRoutes = require("./routes/question_bank");
 var userRoutes     = require("./routes/user");
 var lectureRoutes  = require("./routes/lecture");
 var commentRoutes  = require("./routes/comment");
+var testseriesRoutes = require("./routes/testseries");
 
 app.use("/", indexRoutes);
 app.use("/lecture", lectureRoutes);
 app.use("/question", questionRoutes);
 app.use("/user", userRoutes);
 app.use("/comment", commentRoutes);
+app.use("/testseries", testseriesRoutes);
 
 app.listen('3000', () => {
     console.log('Server started at port 3000');
